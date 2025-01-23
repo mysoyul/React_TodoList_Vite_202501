48ce0f27678d7cb6dca7e75f6c4b867f03644924
mysoyul <vega2k_@naver.com>
2025-01-23 17:58:55 +0900

    react todolist first commit

---
+++ src/components/TodoItemList.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+    render() {
+        const { myTodos, myToggle, myRemove } = this.props;
+        const todoList = myTodos.map(
+            ({ id, text, checked }) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={myToggle}
+                    onRemove={myRemove}
+                    key={id}
+                />
+            )
+        );
+
+        return (
+            <div>
+                {todoList}
+            </div>
+        );
+    }
+}
+TodoItemList.propTypes = {
+    myTodos: PropTypes.array,
+    myToggle: PropTypes.func,
+    myRemove: PropTypes.func
+};
+export default TodoItemList;
