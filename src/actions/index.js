2d29d7f89e8e3fe97522dada427387d8312fa299
mysoyul <vega2k_@naver.com>
2025-01-24 14:56:39 +0900

    action fetchAllTodos add

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,4 +3,20 @@ import axios from 'axios';
 //Action type 정의
 export const FETCH_TODOS = "FETCH_TODOS";
 
-const apiUrl = 'http://localhost:4500/api/todos';
+const apiUrl = import.meta.env.VITE_API_URL;
+//'http://localhost:4500/api/todos';
+
+//Todo 목록 Action 함수
+export const fetchAllTodos = () => {
+    return (dispatch) => {
+        axios.get(apiUrl)
+            .then(res => dispatch({
+                type: FETCH_TODOS,
+                payload: res.data
+            }))
+            .catch(error => {
+                console.log(error);
+                throw error;
+            });            
+    }
+}; //fetchAllTodos
