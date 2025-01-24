cd649314f364831dd09a9e648d71525d8727a3b5
mysoyul <vega2k_@naver.com>
2025-01-24 16:12:43 +0900

    Form.js 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,11 @@ class App extends Component {
   id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
   //상태변수를 포함하고 있는 state 객체
   state = {
-    //Form 하위 Input 엘리먼트에서 입력한 값을 저장하는 변수
-    todo: '',
     todos: [
       { id: 0, text: '리액트 소개', checked: false },
       { id: 1, text: '리액트 구조', checked: true },
       { id: 2, text: '리액트 사용', checked: false }
     ]
-  };
-  //이벤트핸들러 함수 선언
-  handleChange = (e) => {
-    this.setState({
-      todo: e.target.value // input field의 다음 바뀔 값
-    });
-  }
-
-  handleCreate = () => {
-    const { todo, todos } = this.state;
-    const newTodo = {
-      id: this.id++,
-      text: todo,
-      checked: false
-    };
-
-    this.setState({
-      todos: [...todos, newTodo],
-      todo: '', // input 초기화
-    });
-  }
-
-  handleEnter = (e) => {
-    // 눌려진 키가 Enter Key 이면 handleCreate 호출
-    if (e.keyCode === 13) {
-      this.handleCreate();
-    }
   };
 
   handleToggle = (id) => {
@@ -58,17 +29,10 @@ class App extends Component {
   };
 
   render() {
-    const { todo } = this.state;
-    const { handleChange, handleCreate, handleEnter, handleToggle, handleRemove } = this;
+    const { handleToggle, handleRemove } = this;
 
     return (
-      <TodoListTemplate form=
-        {<Form
-          myTodo={todo}
-          myEnter={handleEnter}
-          myChange={handleChange}
-          myCreate={handleCreate} />
-        }>
+      <TodoListTemplate form={<Form />}>
         <TodoItemList myToggle={handleToggle}
           myRemove={handleRemove}
         />
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,14 +2,51 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Form.css';
 
+import { connect } from 'react-redux';
+import { addTodo } from '@/actions';
+
 class Form extends Component {
+    state = {
+        todo: ''
+    }
+
+    //이벤트핸들러 함수 선언
+    handleChange = (e) => {
+        this.setState({
+            todo: e.target.value // input field의 다음 바뀔 값
+        });
+    }
+
+    handleCreate = () => {
+        const { todo } = this.state;
+        const newTodo = {
+            text: todo,
+            checked: false
+        };
+        //props 받은 addTodo action함수 호출하기
+        this.props.addTodo(newTodo);
+
+        this.setState({
+            todo: '', // input 초기화
+        });
+    }
+
+    handleEnter = (e) => {
+        // 눌려진 키가 Enter Key 이면 handleCreate 호출
+        if (e.keyCode === 13) {
+            this.handleCreate();
+        }
+    };
+
     render() {
-        const { myTodo, myEnter, myChange, myCreate } = this.props;
+        const { todo } = this.state;
+        const { handleChange, handleCreate, handleEnter } = this;
+
         return (
             <div className="form">
-                <input value={myTodo} onChange={myChange}
-                    onKeyDown={myEnter} />
-                <div className="create-button" onClick={myCreate}>
+                <input value={todo} onChange={handleChange}
+                    onKeyDown={handleEnter} />
+                <div className="create-button" onClick={handleCreate}>
                     추가
                 </div>
             </div>
@@ -17,9 +54,6 @@ class Form extends Component {
     }
 }
 Form.propTypes = {
-    myTodo: PropTypes.string,
-    myEnter: PropTypes.func,
-    myChange: PropTypes.func,
-    myCreate: PropTypes.func
+    addTodo: PropTypes.func
 };
-export default Form;
+export default connect(null, { addTodo })(Form);
